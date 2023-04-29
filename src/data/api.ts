cba63b549928c6e8bb25bf626cb0e09f066e1b6f
Ricardo Allende <ricardo.allende.p@gmail.com>
2023-04-28 20:59:34 -0600

    Add lesson 10

--- src/data/api.ts
+++ src/data/api.ts
@@ -5,8 +5,8 @@ export const getAllLessons = (): lessonI[] => [
     title: "Preliminar 1. ¿Qué significa la salvación realmente para mí?",
     incompletedUrl:
       "https://drive.google.com/uc?export=download&id=1WMNLHM6JeX7l8XRCI0BnSiT9Nztx0211",
-    originalIncompletedUrl:
-      "https://docs.google.com/document/d/1WMNLHM6JeX7l8XRCI0BnSiT9Nztx0211/edit?usp=share_link&ouid=100689299250752769999&rtpof=true&sd=true",
+    // originalIncompletedUrl:
+    // "https://docs.google.com/document/d/1WMNLHM6JeX7l8XRCI0BnSiT9Nztx0211/edit?usp=share_link&ouid=100689299250752769999&rtpof=true&sd=true",
     completedUrl: "",
     verseToMemorize: "Juan 1:12",
     img: "https://www.enlace.org/wp-content/uploads/2013/04/salvacion.jpg",
@@ -16,8 +16,8 @@ export const getAllLessons = (): lessonI[] => [
     title: "Preliminar 2. ¿Qué espera Dios de mí?",
     incompletedUrl:
       "https://drive.google.com/uc?export=download&id=1VksMHVestS8elUfN1wKo7I-Ya7PEonyC",
-    originalIncompletedUrl:
-      "https://docs.google.com/document/d/1VksMHVestS8elUfN1wKo7I-Ya7PEonyC/edit?usp=share_link&ouid=100689299250752769999&rtpof=true&sd=true",
+    // originalIncompletedUrl:
+    // "https://docs.google.com/document/d/1VksMHVestS8elUfN1wKo7I-Ya7PEonyC/edit?usp=share_link&ouid=100689299250752769999&rtpof=true&sd=true",
     completedUrl: "",
     verseToMemorize: "-",
     img: "https://iglesiabautistalacasadedios.files.wordpress.com/2012/07/vol_05.jpg",
@@ -27,8 +27,8 @@ export const getAllLessons = (): lessonI[] => [
     title: "Lección 1. La seguridad de la salvación",
     incompletedUrl:
       "https://drive.google.com/uc?export=download&id=1KzcHM8ZzT6-PLBoc5_LOj0JdJRmRn_aI",
-    originalIncompletedUrl:
-      "https://docs.google.com/document/d/1KzcHM8ZzT6-PLBoc5_LOj0JdJRmRn_aI/edit?usp=share_link&ouid=100689299250752769999&rtpof=true&sd=true",
+    // originalIncompletedUrl:
+    // "https://docs.google.com/document/d/1KzcHM8ZzT6-PLBoc5_LOj0JdJRmRn_aI/edit?usp=share_link&ouid=100689299250752769999&rtpof=true&sd=true",
     completedUrl: "",
     verseToMemorize: "1 Juan 5:11,12",
     img: "https://images.squarespace-cdn.com/content/v1/5148b380e4b0106646129f8e/1505910962691-74T3DRLOSO6S2LK7QX7I/0848-seguridad-de-salvacion.jpg",
@@ -38,8 +38,8 @@ export const getAllLessons = (): lessonI[] => [
     title: "Lección 2. La biblia",
     incompletedUrl:
       "https://drive.google.com/uc?export=download&id=1I75h6f798c63J-fq9lUGuWxI3R-eM_7o",
-    originalIncompletedUrl:
-      "https://docs.google.com/document/d/1I75h6f798c63J-fq9lUGuWxI3R-eM_7o/edit?usp=share_link&ouid=100689299250752769999&rtpof=true&sd=true",
+    // originalIncompletedUrl:
+    // "https://docs.google.com/document/d/1I75h6f798c63J-fq9lUGuWxI3R-eM_7o/edit?usp=share_link&ouid=100689299250752769999&rtpof=true&sd=true",
     completedUrl: "",
     verseToMemorize: "Salmos 119:9,11",
     img: "https://theobjective.com/wp-content/uploads/2021/02/12114026/la-biblia-y-los-escritores.jpg",
@@ -49,8 +49,8 @@ export const getAllLessons = (): lessonI[] => [
     title: "Lección 3. La iglesia",
     incompletedUrl:
       "https://drive.google.com/uc?export=download&id=1JkT1oDufETlX0qCVZSDhLDl1Mt_EOzTE",
-    originalIncompletedUrl:
-      "https://docs.google.com/document/d/1JkT1oDufETlX0qCVZSDhLDl1Mt_EOzTE/edit?usp=share_link&ouid=100689299250752769999&rtpof=true&sd=true",
+    // originalIncompletedUrl:
+    // "https://docs.google.com/document/d/1JkT1oDufETlX0qCVZSDhLDl1Mt_EOzTE/edit?usp=share_link&ouid=100689299250752769999&rtpof=true&sd=true",
     completedUrl: "",
     verseToMemorize: "Hebreos 10:25",
     img: "ibmtf.jpeg",
@@ -60,8 +60,8 @@ export const getAllLessons = (): lessonI[] => [
     title: "Lección 4. El caminar diario",
     incompletedUrl:
       "https://drive.google.com/uc?export=download&id=1BrjDzOvquD98qgCODXKUsZEGXUQ7G890",
-    originalIncompletedUrl:
-      "https://docs.google.com/document/d/1BrjDzOvquD98qgCODXKUsZEGXUQ7G890/edit?usp=share_link&ouid=100689299250752769999&rtpof=true&sd=true",
+    // originalIncompletedUrl:
+    // "https://docs.google.com/document/d/1BrjDzOvquD98qgCODXKUsZEGXUQ7G890/edit?usp=share_link&ouid=100689299250752769999&rtpof=true&sd=true",
     completedUrl: "",
     verseToMemorize: `Juan 1:12; 1 Juan 5:11,12, Salmo 119:9,11, Hebreos 10:25`,
     img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRAJ3OZitDR_2RvH7Z5UEuf0NRkEJOsSl4Zvg&usqp=CAU",
@@ -71,8 +71,8 @@ export const getAllLessons = (): lessonI[] => [
     title: "Lección 5. El enemigo (Satanás)",
     incompletedUrl:
       "https://drive.google.com/uc?export=download&id=1AEOKReNAuMcaKHQbo2zOc9yV6w9JwYxH",
-    originalIncompletedUrl:
-      "https://docs.google.com/document/d/1AEOKReNAuMcaKHQbo2zOc9yV6w9JwYxH/edit?usp=share_link&ouid=100689299250752769999&rtpof=true&sd=true",
+    // originalIncompletedUrl:
+    // "https://docs.google.com/document/d/1AEOKReNAuMcaKHQbo2zOc9yV6w9JwYxH/edit?usp=share_link&ouid=100689299250752769999&rtpof=true&sd=true",
     completedUrl: "",
     verseToMemorize: `1 Pedro 5:8`,
     img: "https://pbs.twimg.com/media/EUiBE0eWAAE-QS4?format=jpg&name=small",
@@ -82,8 +82,8 @@ export const getAllLessons = (): lessonI[] => [
     title: "Lección 6. La familia (el hogar cristiano)",
     incompletedUrl:
       "https://drive.google.com/uc?export=download&id=1ID8d7o1BOAGp-hKbLQ7dLuPasgxTE8sO",
-    originalIncompletedUrl:
-      "https://docs.google.com/document/d/1ID8d7o1BOAGp-hKbLQ7dLuPasgxTE8sO/edit?usp=share_link&ouid=100689299250752769999&rtpof=true&sd=true",
+    // originalIncompletedUrl:
+    // "https://docs.google.com/document/d/1ID8d7o1BOAGp-hKbLQ7dLuPasgxTE8sO/edit?usp=share_link&ouid=100689299250752769999&rtpof=true&sd=true",
     completedUrl: "",
     verseToMemorize: `Josué 24:15`,
     img: "https://assets.website-files.com/5ed9667eb17cb440badef321/612e7052a8d4793ed5c01f50_Juntos%20somos%20una%20gran%20familia%20padres%20de%20familia%20y%20escuela.png",
@@ -93,8 +93,8 @@ export const getAllLessons = (): lessonI[] => [
     title: "Lección 7. Dios",
     incompletedUrl:
       "https://drive.google.com/uc?export=download&id=1n-71lYE4gyap1bGhyECsN0I2bftB0Lo6",
-    originalIncompletedUrl:
-      "https://docs.google.com/document/d/1n-71lYE4gyap1bGhyECsN0I2bftB0Lo6/edit?usp=share_link&ouid=100689299250752769999&rtpof=true&sd=true",
+    // originalIncompletedUrl:
+    // "https://docs.google.com/document/d/1n-71lYE4gyap1bGhyECsN0I2bftB0Lo6/edit?usp=share_link&ouid=100689299250752769999&rtpof=true&sd=true",
     completedUrl: "",
     verseToMemorize: `Hebreos 11:6`,
     img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQqpRGNFLZJkALbkgOrP_iuXrkkj7LcymHX_ICYqRYCacZ6mz0EcmRVIJVc6WG7pI9b8MQ&usqp=CAU",
@@ -106,8 +106,8 @@ export const getAllLessons = (): lessonI[] => [
     title: "Lección 8. El Espíritu Santo",
     incompletedUrl:
       "https://drive.google.com/uc?export=download&id=119HrjCzt4tIs0eEEQnH8j9dZda_Ae8OC",
-    originalIncompletedUrl:
-      "https://docs.google.com/document/d/119HrjCzt4tIs0eEEQnH8j9dZda_Ae8OC/edit?usp=share_link&ouid=100689299250752769999&rtpof=true&sd=true",
+    // originalIncompletedUrl:
+    // "https://docs.google.com/document/d/119HrjCzt4tIs0eEEQnH8j9dZda_Ae8OC/edit?usp=share_link&ouid=100689299250752769999&rtpof=true&sd=true",
     completedUrl: "",
     verseToMemorize: `Efesios 5.18`,
     img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRovAuFeEQ3JHzJWtbdGQUPcYAtg5WeMPOTOA&usqp=CAU",
@@ -117,11 +117,22 @@ export const getAllLessons = (): lessonI[] => [
     title: "Lección 9. La inmersión (el bautismo)",
     incompletedUrl:
       "https://drive.google.com/uc?export=download&id=10rQSGrGFxWU0LXDIBcE_h32gW_Evdkin",
-    originalIncompletedUrl:
-      "https://docs.google.com/document/d/10rQSGrGFxWU0LXDIBcE_h32gW_Evdkin/edit?usp=sharing&ouid=100689299250752769999&rtpof=true&sd=true",
+    // originalIncompletedUrl:
+    // "https://docs.google.com/document/d/10rQSGrGFxWU0LXDIBcE_h32gW_Evdkin/edit?usp=sharing&ouid=100689299250752769999&rtpof=true&sd=true",
     completedUrl: "",
     verseToMemorize: `Hechos 2:41`,
     img: "https://sallomo.es/wp-content/uploads/2016/02/el-bautismo-de-jesus.jpg",
+    videos: [],
+  },
+  {
+    title: "Lección 10. Jesucristo",
+    incompletedUrl:
+      "https://drive.google.com/uc?export=download&id=1phVjQd14H08kkLlbsDHshrRpCYpceInW",
+    // originalIncompletedUrl:
+    // "https://docs.google.com/document/d/1phVjQd14H08kkLlbsDHshrRpCYpceInW/edit?usp=share_link&ouid=100689299250752769999&rtpof=true&sd=true",
+    completedUrl: "",
+    verseToMemorize: `Juan 1:1`,
+    img: "https://m.media-amazon.com/images/I/41QwH8vmuUL._AC_UF1000,1000_QL80_.jpg",
     videos: [],
   },
 ];
